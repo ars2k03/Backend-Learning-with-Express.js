cfc32ea3a6b4f2d96840a8ba6a162b7a19e5dd76
ars2k03 <arafatsohan2003@gamil.com>
2026-05-16 14:13:19 +0600

    Connect MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,47 @@
 import express, { type Application, type NextFunction, type Request, type Response } from "express";
-import file from './Database/data.json' with { type: 'json' };
 import fs from "fs";
+import mongoose from "mongoose";
 
 const app : Application = express();
 const port : number = 8000;
+
+//Connect MongoDB
+
+mongoose.connect('mongodb://127.0.0.1:27017/ARS')
+.then(() => console.log("MongoDB is Connected Succesfully")
+).catch((e) => console.log(`Connection Failed : ${e}`));
+
+//Schema
+
+const userSchema = new mongoose.Schema({
+  first_name : {
+    type : String,
+    required : true,
+  },
+
+  last_name : {
+    type : String,
+    required : true,
+  },
+
+  email : {
+    type : String,
+    required : true,
+    unique : true,
+  },
+
+  gender : {
+    type : String,
+    required : true,
+  },
+
+  job_title : {
+    type : String,
+    required : true
+  }
+}, {timestamps : true});
+
+const User = mongoose.model('user', userSchema);
 
 //MiddleWare - Plugins
 app.use(express.urlencoded({extended : false}));
@@ -21,7 +59,8 @@ app.use((req : Request, res : Response, next : NextFunction) => {
   })
 })
 
-app.get('/users', (req : Request, res : Response) => {
+app.get('/users', async (req : Request, res : Response) => {
+  const file = await User.find({});
   const html = `
   <ul>
     ${file.map(user => `<li>${user.first_name}</li>`).join("")}
@@ -31,37 +70,42 @@ app.get('/users', (req : Request, res : Response) => {
 })
 
 //REST API
-app.route('/api/users').get((req : Request, res : Response) => {
+app.route('/api/users')
+.get( async (req : Request, res : Response) => {
+  const user = await User.find({})
   res.setHeader('X-Me', 'A R S Arafat');
-
-  console.log(req.headers);
-  
-  return res.status(200).json(file);
+  return res.status(200).json(user);
 })
-.post((req : Request, res : Response) => {
+.post(async (req : Request, res : Response) => {
   const body = req.body;
-  file.push({
-    id : file.length + 1,
-    ...body,
-  });
-
-  fs.writeFile('./src/Database/data.json', JSON.stringify(file), (error) => {
-    if(error){
-      return res.status(500).json({
-        message : "Failed to Created Data"
-      })
-    }
-    return res.status(201).json({
-      message : "Created Successfully", 
-      data : body,
+  if(!body.first_name || 
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ){
+    return res.status(404).json({
+      message : "All Field are Required"
     })
+  }
+
+  const result = await User.create({
+    first_name : body.first_name,
+    last_name : body.last_name,
+    email : body.email,
+    gender : body.gender,
+    job_title : body.job_title
+  })
+
+  return res.status(201).json({
+    message : "Success",
+    data : result
   })
 })
 
 app.route('/api/users/:id')
-.get((req : Request, res : Response) => {
-  const id = req.params.id;
-  const user = file.find(person => person.id === Number(id));
+.get(async (req : Request, res : Response) => {
+  const user = await User.findById(req.params.id);
 
   if(!user) {
     return res.status(404).json({
@@ -71,62 +115,37 @@ app.route('/api/users/:id')
 
   return res.status(200).json(user);
 })
-.patch((req: Request, res: Response) => {
-  const id = Number(req.params.id);
+.patch( async (req: Request, res: Response) => {
   const body = req.body;
 
-  const userIndex = file.findIndex(user => user.id === id);
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, body, {
+    new: true,
+    runValidators: true,
+  });
 
-  if (userIndex === -1) {
+  if (!updatedUser) {
     return res.status(404).json({
       message: "User not found"
     });
   }
 
-  file[userIndex] = {
-    ...file[userIndex],
-    ...body,
-  };
-
-  fs.writeFile('./src/Database/data.json', JSON.stringify(file, null, 2), (error) => {
-    if (error) {
-      return res.status(500).json({
-        message: "Failed to update user"
-      });
-    }
-
-    return res.status(200).json({
-      message: "User updated successfully",
-      data: body
-    });
+  return res.status(200).json({
+    message: "User updated successfully",
+    data: updatedUser
   });
 })
-.delete((req: Request, res: Response) => {
-  const id = Number(req.params.id);
+.delete(async (req: Request, res: Response) => {
+  const deletedUser = await User.findByIdAndDelete(req.params.id);
 
-  const userIndex = file.findIndex(user => user.id === id);
-
-  if (userIndex === -1) {
+  if (!deletedUser) {
     return res.status(404).json({
       message: "User not found"
     });
   }
 
-  const deletedUser = file[userIndex];
-
-  file.splice(userIndex, 1);
-
-  fs.writeFile('./src/Database/data.json', JSON.stringify(file), (error) => {
-    if (error) {
-      return res.status(500).json({
-        message: "Failed to delete user"
-      });
-    }
-
-    return res.status(200).json({
-      message: "User deleted successfully",
-      data: deletedUser
-    });
+  return res.status(200).json({
+    message: "User deleted successfully",
+    data: deletedUser
   });
 })
 
